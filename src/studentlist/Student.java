5462b587b551f116b062564938f14379e05176b7
Shaimaa-Ali-Sheridan <shaimaa.ali1@sheridancollege.ca>
2019-01-23 20:40:54 -0500

    Removing the DoB and its setters and getters

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -14,8 +14,8 @@ public class Student {
  
     private String name;
     private int id;
-    private String DoB;
-      
+    
+   
     public Student()
     {
            this.name = "Not Set";
@@ -42,20 +42,6 @@ public class Student {
     public int getId()
     {
            return this.id;
-    }
-
-    /**
-     * @return the DoB
-     */
-    public String getDoB() {
-        return DoB;
-    }
-
-    /**
-     * @param DoB the DoB to set
-     */
-    public void setDoB(String DoB) {
-        this.DoB = DoB;
     }
             
     
